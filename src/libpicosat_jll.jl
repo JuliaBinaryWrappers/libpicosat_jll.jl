c7f7ba8aae2556403a8225402ac27804af584f3e
jlbuild <juliabuildbot@gmail.com>
2026-08-17 18:53:33 +0000

    libpicosat_jll build 965.0.2+0

--- src/libpicosat_jll.jl
+++ src/libpicosat_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libpicosat")
-JLLWrappers.@generate_main_file("libpicosat", UUID("6b231c3b-13f8-5ced-86ae-8860c7f75d86"))
+JLLWrappers.@generate_main_file("libpicosat", Base.UUID("6b231c3b-13f8-5ced-86ae-8860c7f75d86"))
 end  # module libpicosat_jll
